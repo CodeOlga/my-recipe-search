4ea1c208b1e4272992a38d29d28b1a47f5276bd6
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-12-08 14:18:35 +0200

    first commit

--- src/MyRecipesComponent.js
+++ src/MyRecipesComponent.js
@@ -1,24 +1,28 @@
 function MyRecipesComponent({label, image, cuisineType, mealType, calories, ingredients}) {
   return(
     <div>
-      <div className="container">
-        <h2>{label}</h2>
-      </div>
+        <div className="card">
+          <div className="container">
+            <h2>{label}</h2>
+          </div>
 
-      <div className="container">
-        <img className="tasty" src={image} alt="dish" />
-      </div>
+          <div className="container">
+            <img className="tasty" src={image} alt="dish" />
+          </div>
 
-      <div className="container">
-        <p className="type">{cuisineType}</p>
-      </div>
-      <div className="container">
-        <p className="type">{mealType}</p>
-      </div>
-      <div className="container">
-        <p className="type">{calories.toFixed()} calories</p>
-      </div>
-
+          <div className="container">
+            <p className="type">{cuisineType}</p>
+          </div>
+      
+          <div className="container">
+            <p className="type">{mealType}</p>
+          </div>
+      
+          <div className="container">
+            <p className="type">{calories.toFixed()} calories</p>
+          </div>
+        </div>
+        
       <ul className="list">
         {ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient}</li>
